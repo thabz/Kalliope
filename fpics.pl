0f3b543ff495636fccb3e27c5109da71ed3b62f9
Jesper Christensen <jesper@kalliope.org>
2001-01-07 19:51:14 +0000

    GPL boilerplate

--- fpics.pl
+++ fpics.pl
@@ -1,5 +1,25 @@
 #!/usr/bin/perl
 
+#  Copyright (C) 1999-2001 Jesper Christensen 
+#
+#  This script is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation; either version 2 of the
+#  License, or (at your option) any later version.
+#
+#  This script is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this script; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+#  Author: Jesper Christensen <jesper@kalliope.org>
+#
+#  $Id$
+
 use Kalliope;
 do 'fstdhead.pl';
 
@@ -11,13 +31,9 @@ chop($fhandle);
 chomp($LA);
 fheaderHTML($fhandle);
 
-#Begynd kasse
-print "<FONT SIZE=3><BR>\n";
-
 beginwhitebox("Portrætter","","center");
 
 $i=1;
-print "<CENTER><FONT SIZE=2>\n";
 print "<TABLE><TR>";
 while (-e "fdirs/".$fhandle."/p".$i.".jpg") {
     print '<TD WIDTH="33%" VALIGN="top" ALIGN="center">';
@@ -37,13 +53,6 @@ while (-e "fdirs/".$fhandle."/p".$i.".jpg") {
     }
 }
 print "</TR></TABLE>";
-#if ($i==2) {
-#    print "<BR><BR><I>Klik på billedet for at forstørre det</I>";
-#} else {
-#    print "<BR><BR><I>Klik på billed</I>";
-#}
-
-print "</FONT></CENTER>";
 
 endbox();
 ffooterHTML();
