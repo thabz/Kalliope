83686317e7ee9b921b946d7612db9be0ace55202
Jesper Christensen <jesper@kalliope.org>
2001-06-11 17:40:31 +0000

    Ignore case <w> og <W>

--- Kalliope/Poem.pm
+++ Kalliope/Poem.pm
@@ -139,8 +139,8 @@ sub content {
     } else {
 	$self->{'indhold'} =~ s/ /&nbsp;/;
     }
-    $self->{'indhold'} =~ s/<w>/<span class="wide">/g;
-    $self->{'indhold'} =~ s/<\/w>/<\/span>/g;
+    $self->{'indhold'} =~ s/<w>/<span class="wide">/gi;
+    $self->{'indhold'} =~ s/<\/w>/<\/span>/gi;
     $self->{'indhold'} =~ s/\n/<BR>\n/g;
     $self->{'indhold'} =~ s/,,/&bdquo;/g;
     $self->{'indhold'} =~ s/''/&ldquo;/g;
