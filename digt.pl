ad19bbdc6ca15d8e9d69b128427c9dceb4bdb8a5
Jesper Christensen <jesper@kalliope.org>
2001-06-15 20:34:11 +0000

    NAME attributten i FORM er deprecated iflg. W3C.

--- digt.pl
+++ digt.pl
@@ -205,7 +205,7 @@ sub keywords {
         $HTML = 'Dette digt har endnu ingen nøgleord tilknyttet.';
     }
     my $longdid = $poem->longdid;
-    $HTML .= qq|<br><BR>Tilføj nøgleord<BR><FORM METHOD="get" NAME="addkeyword"><INPUT TYPE="text" CLASS="inputtext" NAME="newkeywords"><INPUT TYPE="hidden" NAME="longdid" VALUE="$longdid"><INPUT TYPE="submit" CLASS="button" VALUE="Tilføj"> |;
+    $HTML .= qq|<br><BR>Tilføj nøgleord<BR><FORM METHOD="get"><INPUT TYPE="text" CLASS="inputtext" NAME="newkeywords"><INPUT TYPE="hidden" NAME="longdid" VALUE="$longdid"><INPUT TYPE="submit" CLASS="button" VALUE="Tilføj"> |;
     $HTML .= Kalliope::Help->new('keywordadd')->linkAsHTML;
     $HTML .= '</FORM>';
     return $HTML;
