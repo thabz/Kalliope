d9045835e85d59ecd26a31d1dce4551fe530745f
Jesper Christensen <jesper@kalliope.org>
2001-08-13 20:46:04 +0000

    Brug DB istedet for fnavne.txt filer

--- makethumbs.pl
+++ makethumbs.pl
@@ -1,32 +1,27 @@
 #!/usr/bin/perl
 
-#Make thumbnails of all images in fdirs
+# Make thumbnails of all images in fdirs
 
-do_country("dk");
-do_country("uk");
-do_country("fr");
-do_country("de");
-do_country("se");
-do_country("no");
+use Kalliope::DB;
 
-sub do_country {
-    open(FILE,"data.$_[0]/fnavne.txt");
-    while(<FILE>) {
-	($fhandle) = split(/=/);
-	if (-e "fdirs/$fhandle") {
-	    print $fhandle."\n";
-	    $i = 1;
-	    while (-e "fdirs/$fhandle/p$i.jpg") {
-		if (!(-e "fdirs/$fhandle/_p$i.jpg" )) {
-		    print "    Making thumb for $fhandle picture #$i\n";
-			system ("convert -geometry x150 fdirs/$fhandle/p$i.jpg fdirs/$fhandle/_p$i.jpg"); 
-		    
-		}
-		$i++;
+my $dbh = Kalliope::DB::connect();
+
+my $sth = $dbh->prepare("SELECT fhandle FROM fnavne");
+$sth->execute();
+
+while ($fhandle = $sth->fetchrow_array) {
+    if (-e "fdirs/$fhandle") {
+	print $fhandle."\n";
+	$i = 1;
+	while (-e "fdirs/$fhandle/p$i.jpg") {
+	    if (!(-e "fdirs/$fhandle/_p$i.jpg" )) {
+		print "    Making thumb for $fhandle picture #$i\n";
+		system ("convert -geometry x150 fdirs/$fhandle/p$i.jpg fdirs/$fhandle/_p$i.jpg"); 
+
 	    }
+	    $i++;
 	}
     }
-    close(FILE);
 }
 
 
