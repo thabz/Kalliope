7300d87c979588c42191e17653f71aacdab5be50
Jesper Christensen <jesper@kalliope.org>
2001-11-16 14:04:11 +0000

    favicon virker også i Mozilla nu

--- Kalliope/Page.pm
+++ Kalliope/Page.pm
@@ -218,6 +218,7 @@ sub print {
     print '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">';
     print <<"EOF";
 <HTML><HEAD><TITLE>$titleForWindow</TITLE>
+<LINK REL="Shortcut Icon" HREF="http://www.kalliope.org/favicon.ico">
 <LINK REL=STYLESHEET TYPE="text/css" HREF="kalliope.css">
 <META HTTP-EQUIV="Content-Type" content="text/html; charset=iso-8859-1">
 <META name="description" content="Stort arkiv for ældre digtning">
