358c30e2f1a922f153fc01a044b2b66e7d427f24
Jesper Christensen <jesper@kalliope.org>
2001-11-18 13:23:11 +0000

    IRC bot lagt i CVS

---
+++ bot/src/KalliopePerson.pl
@@ -0,0 +1,24 @@
+
+package KalliopePerson;
+
+use lib "..";
+
+use Kalliope::Person;
+
+sub get { 
+    my $line = shift;
+    unless ($line =~ /^person/i) { 
+	return 'Øh?';
+   }
+
+   my ($fhandle) = $line =~ /person *([^ ]+) *$/;
+   return 'Hvilket navn ville du have info for?' unless $fhandle;
+
+   return "Jeg kender ingen $fhandle ..." unless Kalliope::Person::exist($fhandle);
+
+   my $person = new Kalliope::Person (fhandle => $fhandle);
+
+   return $person->name." ".$person->lifespan;
+}
+
+1;
