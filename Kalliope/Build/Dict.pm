ea9979c4fd06a804474a60051f3c4bee9cc15d7d
Jesper Christensen <jesper@kalliope.org>
2002-01-12 13:39:23 +0000

    Samme <ord> ryger i samme opslag

--- Kalliope/Build/Dict.pm
+++ Kalliope/Build/Dict.pm
@@ -21,7 +21,7 @@
 #  $Id$
 
 package Kalliope::Build::Dict;
-    
+
 use XML::DOM;
 require Unicode::String;
 use Kalliope::DB;
@@ -32,92 +32,111 @@ use strict;
 my $dbh = Kalliope::DB::connect();
 
 sub parse {
-   my $filename = shift;
-   my $parser = new XML::DOM::Parser;
-   my $doc = $parser->parsefile($filename);
- 
-   my @dict;
-   my $entries = $doc->getElementsByTagName('entry');
-   my $n = $entries ->getLength;
+    my $filename = shift;
+    my $parser = new XML::DOM::Parser;
+    my $doc = $parser->parsefile($filename);
 
-   my $lastOK;
-   for (my $i = 0; $i < $n; $i++) {
-       my $p;
-       my $entry = $entries ->item($i);
-       my %dentry;
+    my %dict;
+    my $entries = $doc->getElementsByTagName('entry');
+    my $n = $entries ->getLength;
 
-       $dentry{'id'} = $entry->getAttribute('id');
+    my $lastOK;
+    for (my $i = 0; $i < $n; $i++) {
+	my $p;
+	my $entry = $entries ->item($i);
+	my %dentry;
 
-       if ($entry->getElementsByTagName('ord')->item(0)) {
-          $dentry{'ord'} = Unicode::String::utf8($entry->getElementsByTagName('ord')->item(0)->getFirstChild->getNodeValue)->latin1;
-          $lastOK = $dentry{'ord'};
-       } else {
-          print $lastOK;
-          exit;
-       }
+	$dentry{'id'} = $entry->getAttribute('id');
 
-       $dentry{'forkl'} = '';
+	if ($entry->getElementsByTagName('ord')->item(0)) {
+	    $dentry{'ord'} = Unicode::String::utf8($entry->getElementsByTagName('ord')->item(0)->getFirstChild->getNodeValue)->latin1;
+	    $lastOK = $dentry{'ord'};
+	} else {
+	    print $lastOK;
+	    exit;
+	}
 
-       if ($entry->getElementsByTagName('frase')->item(0)) {
-          $dentry{'forkl'} .= '<span style="color: #808080">('.Unicode::String::utf8(
-		  $entry->getElementsByTagName('frase')->item(0)->toString)->latin1.
-                 ')</span> ';
-       }
+	$dentry{'forkl'} = '';
 
-       if ($entry->getElementsByTagName('forkl')->item(0)) {
-          $dentry{'forkl'} .= Unicode::String::utf8($entry->getElementsByTagName('forkl')->item(0)->toString)->latin1;
-       } else {
-       }
+	if ($entry->getElementsByTagName('frase')->item(0)) {
+	    $dentry{'forkl'} .= '<span style="color: #808080">('.Unicode::String::utf8(
+			$entry->getElementsByTagName('frase')->item(0)->toString)->latin1.
+		    ')</span> ';
+	}
 
-       push @dict,\%dentry;
+	if ($entry->getElementsByTagName('forkl')->item(0)) {
+	    $dentry{'forkl'} .= Unicode::String::utf8($entry->getElementsByTagName('forkl')->item(0)->toString)->latin1;
+	} else {
+	}
 
-       if ($entry->getElementsByTagName('var')->item(0)) {
-           my $vars = $entry->getElementsByTagName('var');
-           my $nn = $vars->getLength;
-           for (my $j = 0; $j < $nn; $j++) {
-               my %dentryVar;
-               my $var = Unicode::String::utf8($vars->item($j)->getFirstChild->getNodeValue)->latin1;
-               $dentryVar{'id'} = $dentry{'ord'}.$var;
-               $dentryVar{'ord'} = $var;
-               $dentryVar{'forkl'} = 'Se <A CLASS="green" HREF="dict.cgi?wid='.$dentry{'id'}.'">'.
-                                      $dentry{'ord'}.'</A>';
-               push @dict,\%dentryVar;
-           }
-       }
-   }
-   return @dict;
+	addToDict(\%dentry);
+
+	if ($entry->getElementsByTagName('var')->item(0)) {
+	    my $vars = $entry->getElementsByTagName('var');
+	    my $nn = $vars->getLength;
+	    for (my $j = 0; $j < $nn; $j++) {
+		my %dentryVar;
+		my $var = Unicode::String::utf8($vars->item($j)->getFirstChild->getNodeValue)->latin1;
+		$dentryVar{'id'} = $dentry{'ord'}.$var;
+		$dentryVar{'ord'} = $var;
+		$dentryVar{'forkl'} = 'Se <A CLASS="green" HREF="dict.cgi?wid='.$dentry{'id'}.'">'.
+		    $dentry{'ord'}.'</A>';
+		addToDict(\%dentryVar);
+	    }
+	}
+    }
+    return %dict;
+
+    sub addToDict {
+	my $entry = shift;
+	if ($dict{$entry->{'ord'}}) {
+	    my $old = $dict{$entry->{'ord'}};
+	    my $forkl = $old->{'forkl'};
+	    if ($old->{'count'} == 1) {
+		$forkl = "1. ".$forkl;
+	    }
+	    $old->{'count'}++;
+	    $forkl .= '; '.$old->{'count'}.'. '.$entry->{'forkl'};
+	    $old->{'forkl'} = $forkl;
+	    $dict{$entry->{'ord'}} = $old;
+	} else {
+	    $entry->{'count'} = 1;
+	    $dict{$entry->{'ord'}} = $entry;
+	}
+    }
 }
+
 
 
 sub create {
     $dbh->do("DROP TABLE IF EXISTS dict");
     $dbh->do("CREATE TABLE dict ( 
-              wid char(20) primary key,
-              firstletter char(3),
-              word varchar(127),
-              forkl text,
-              var text,
-              KEY word_index (word(10)), 
-              KEY firstletter_index (firstletter(1)), 
-              UNIQUE (wid))");
+	wid char(20) primary key,
+    firstletter char(3),
+    word varchar(127),
+    forkl text,
+    var text,
+    KEY word_index (word(10)), 
+    KEY firstletter_index (firstletter(1)), 
+    UNIQUE (wid))");
 }
 
 sub insert {
     my $dictRef = shift;
-    my @dict = @{$dictRef};
+    my %dict = %{$dictRef};
 
     my $rc = $dbh->prepare("INSERT INTO dict (wid,firstletter,word,forkl,var) VALUES (?,?,?,?,?)");
-    
-    foreach my $entry (@dict) { 
-        my $ord = $entry->{'ord'};
-        my $fl = lc substr($ord,0,2);
-        $fl = $fl eq 'aa' ? 'å' : lc substr($ord,0,1);
-        
-        $rc->execute($entry->{'id'},
-                     $fl,
-                     $ord,
-                     fixTags($entry->{'forkl'}), 
-                     fixTags($entry->{'var'}));
+
+    foreach my $entry (values %dict) {
+	my $ord = $entry->{'ord'};
+	my $fl = lc substr($ord,0,2);
+	$fl = $fl eq 'aa' ? 'å' : lc substr($ord,0,1);
+
+	$rc->execute($entry->{'id'},
+		$fl,
+		$ord,
+		fixTags($entry->{'forkl'}), 
+		fixTags($entry->{'var'}));
     }
 }
 
