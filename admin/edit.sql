3174f6325a33994dd06e5fcd2a1338e57d221327
Jesper Christensen <jesper@kalliope.org>
2002-03-21 13:05:06 +0000

    SQL til nye edit-system

---
+++ admin/edit.sql
@@ -0,0 +1,29 @@
+# MySQL dump 5.12
+#
+# Host: localhost    Database: kalliope
+#--------------------------------------------------------
+# Server version	3.22.16a-gamma
+
+#
+# Table structure for table 'editpages'
+#
+DROP TABLE IF EXISTS edipages;
+CREATE TABLE editpages (
+	filename varchar(128) NOT NULL,
+	dir varchar(128) NOT NULL,
+	data text,
+        KEY(filename),
+        KEY(dir)
+);
+
+DROP TABLE IF EXISTS edithistory;
+CREATE TABLE edithistory (
+	filename varchar(128) NOT NULL,
+	dir varchar(128) NOT NULL,
+	action enum('edit','accept'),
+        login varchar(64),
+        date int,
+        KEY(filename),
+        KEY(dir)
+);
+
