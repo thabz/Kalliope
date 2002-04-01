5a85763df86d4eada4631b595bf1209449522a6b
Jesper Christensen <jesper@kalliope.org>
2002-04-01 09:02:12 +0000

    Håndter VP: med path i billedets filnavn

--- Kalliope/Work.pm
+++ Kalliope/Work.pm
@@ -111,7 +111,9 @@ sub pics {
    my $fhandle = $self->author->fhandle;
    foreach my $line (split /\$\$\$/,$pics) {
       my ($url,$desc) = split /%/,$line;
-      push @result,{ thumbfile => 'fdirs/'.$fhandle.'/_'.$url,
+      my $thumb = $url;
+      $thumb =~ s/^(.*?)([^\/]+)$/$1_$2/;
+      push @result,{ thumbfile => 'fdirs/'.$fhandle.'/'.$thumb,
                      destfile =>  'fdirs/'.$fhandle.'/'.$url,
                      description => $desc };
    }	     
