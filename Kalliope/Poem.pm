de30b6cb3e4bf82740e39a80c4f1cca3f4890d50
Jesper Christensen <jesper@kalliope.org>
2002-04-09 18:46:25 +0000

    Opfat <note> som <footnote>

--- Kalliope/Poem.pm
+++ Kalliope/Poem.pm
@@ -126,6 +126,8 @@ sub extractFootnotes {
     my ($self,$content) = @_;
     my @footnotes = $self->{'footnotes'} ? @{$self->{'footnotes'}} : ();
     my $num = $#footnotes >= 0 ? $#footnotes + 2 : 1;
+    $content =~ s/<note>/<footnote>/g;
+    $content =~ s/<\/note>/<\/footnote>/g;
     while ($content =~ s/<footnote>(.*?)<\/footnote>/<footmark id="footnote$num"\/>/mi) {
        push @{$self->{'footnotes'}},$1;
        $num++;
