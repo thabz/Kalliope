24fe830cd6f48962043744e90b0de5a676f99a61
Jesper Christensen <jesper@kalliope.org>
2002-11-25 23:19:52 +0000

    Debian friendly

--- cron/makegraph.sh
+++ cron/makegraph.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 ./graph.pl > tal
-/usr/local/bin/gnuplot gplot > plot.png
-/usr/local/bin/convert -transparent white plot.png plot.gif
+/usr/bin/gnuplot gplot > plot.png
+/usr/bin/convert -transparent white plot.png plot.gif
 rm tal plot.png
