30ba9864fc60b6bb6340018a1648e629d7a2c88e
Jesper Christensen <jesper@kalliope.org>
2003-01-12 10:55:21 +0000

    Nyt home interface

---
+++ Kalliope/PoemHome.pm
@@ -0,0 +1,42 @@
+#!/usr/bin/perl -w
+
+#  Copyright (C) 1999-2001 Jesper Christensen 
+#
+#  This script is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation; either version 2 of the
+#  License, or (at your option) any later version.
+#
+#  This script is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this script; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+#  Author: Jesper Christensen <jesper@kalliope.org>
+#
+#  $Id$
+
+package Kalliope::PoemHome;
+
+use Kalliope::Poem;
+
+my %cache;
+
+sub findByLongdid {
+    my ($longdid) = @_;
+    if (defined $cache{$longdid}) {
+       return $cache{$longdid};
+    } else {
+       $cache{$longdid} = new Kalliope::Poem('longdid' => $longdid);
+       return $cache{$longdid};
+    }
+}
+
+
+1;
+
+
