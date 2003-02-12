a35f14d8d3346e73bd03f20d9bc59b5523296d98
Jesper Christensen <jesper@kalliope.org>
2003-02-12 12:38:28 +0000

    Escapes

--- Kalliope.pm
+++ Kalliope.pm
@@ -117,6 +117,7 @@ sub buildhrefs {
     $$txt =~ s/<A\s+D="?([^\s>"]+)"?\s*>/<A HREF="digt.pl?longdid=$1">/gi;
     $$txt =~ s/<A\s+K="?([^\s>"]+)"?\s*>/<A HREF="keyword.cgi?keyword=$1">/gi;
    $$txt =~ s/<A\s+V="?([^\s>\/]+)\/([^\s>\/"]+)"?\s*>/<A HREF="vaerktoc.pl?fhandle=$1&vhandle=$2">/gi;
+   $$txt =~ s/<a\s+work="(.*?)">/<a href="vaerktoc.pl?vid=$1">/gi;
    $$txt =~ s/<A\s+/<A CLASS=green /gi;
    $$txt =~ s/<year>(\d+)<\/year>/<A CLASS="timecontext" TITLE="Vis historisk kontekst for året $1." onClick="return openTimeContext($1)" HREF="javascript:{}">$1<\/A>/gi;
    return $$txt;
