aa9d1585ab809bc9f9cc7c84a4dda6f76e9b4da2
Jesper Christensen <jesper@kalliope.org>
2003-02-14 11:40:26 +0000

    Options understøttet

--- makejoin.pl
+++ makejoin.pl
@@ -1,6 +1,11 @@
 #!/usr/bin/perl -w
 
 use strict;
+use Getopt::Long;
+
+my @notes = ();
+my $quality;
+GetOptions ("note=s" => \@notes, "quality=s" => \$quality);
 
 if ($#ARGV < 1) {
     usage();
@@ -20,25 +25,32 @@ opendir(DIR,$dir) || die $!;
 my @files = grep {!/^\./} readdir(DIR);
 
 foreach my $file (@files) {
-    print header($fhandle,sprintf("%03d",$i),$date);
+    print header($fhandle,sprintf("%03d",$i),$date,$quality,@notes);
     open(FILE,"$dir/$file");
     while (<FILE>) { print $_ };
     print footer();
 }
 
 sub header {
-    my ($fhandle,$i,$date) = @_;
+    my ($fhandle,$i,$date,$quality,@notes) = @_;
+    my $notes = '';
+    if ($#notes >= 0) {
+        $notes = "\n   <notes>\n";
+	foreach my $note (@notes) {
+           $notes .= "      <note>$note</note>\n";
+	}
+        $notes .= "   </notes>";
+    }
+    $quality = qq(\n   <quality>$quality</quality>) if $quality;
+    
     return <<"EOF";
+<!-- ================================================================ -->
 <poem id="$fhandle$date$i">
 <head>
    <title></title>
    <toctitle></toctitle>
    <indextitle></indextitle>
-   <firstline></firstline>
-   <notes>
-      <note></note>
-   </notes>
-   <quality>korrektur1</quality>
+   <firstline></firstline>$notes$quality
 </head>
 <body>
 EOF
@@ -54,7 +66,10 @@ EOF
 
 
 sub usage {
-    print "Usage: makejoin.pl fhandle dir\n";
-    print "Wraps a dir of poem files into xml to create a work\n";
+    print "Usage: makejoin.pl [options] fhandle dir\n";
+    print "Wraps a dir of poem files into xml to create a work\n\n";
+    print "Options:\n";
+    print qq(  --note <string>\t\tAdds a note field to alle poems.\n\t\t\t\tYou can have several --note\n);
+    print qq(  --quality <string>\t\tAdds a quality field poems\n);
 }
 
