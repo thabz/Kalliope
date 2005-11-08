d0e6ca8b42b46b7c2c384edeeac71dcc6ed91d31
Jesper Christensen <jesper@kalliope.org>
2005-11-08 08:32:57 +0000

    Fix

--- admin/makeindex.sh
+++ admin/makeindex.sh
@@ -4,7 +4,6 @@ if test \! -d "dump"; then mkdir "dump"; fi
 
 ./swish/dump2swish.pl
 swish-e -c swish/common.conf -S fs
-rm -fr dump
 
 echo
 echo "Søg med swish-search -f ../index/swish.index -w Foraar"
