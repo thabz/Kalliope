12b400f45d20f1878d236ecf550cb99097211673
Jesper Christensen <jesper@kalliope.org>
2009-04-17 07:31:03 +0000

    Ny tæller

--- Kalliope/Page.pm
+++ Kalliope/Page.pm
@@ -350,12 +350,15 @@ GOOGLEADS
     print '</TABLE></div></center>';
 
     print q|
-    <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-    </script>
     <script type="text/javascript">
-    _uacct = "UA-2135385-1";
-    urchinTracker();
-    </script>|;
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8418639-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>|;
     print '</BODY></HTML>';
 }
 
