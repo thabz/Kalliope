09839c56c354a723f7175c3c53008697d1aa99e8
Jesper Christensen <jesper@kalliope.org>
2012-02-12 10:33:34 +0100

    Mobile ads

--- Kalliope/Page.pm
+++ Kalliope/Page.pm
@@ -84,6 +84,12 @@ sub new {
     return $self;
 }
 
+sub isMobile() {
+   my $agent = $ENV{HTTP_USER_AGENT};
+   return 1 if ($agent =~ m/android|avantgo|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i);
+    return 1 if (substr($agent, 0, 4) =~ m/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|e\-|e\/|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(di|rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|xda(\-|2|g)|yas\-|your|zeto|zte\-/i);
+    return 0;
+}
 sub newAuthor {
     my ($class,%args) = @_;
     my $poet = $args{'poet'};
@@ -300,10 +306,11 @@ sub print {
 	    print qq|<html><head><meta http-equiv="refresh" content="0; URL=$url"></head></html>|;
 	    return;
     }
-    
+    my $http_accept_language = $ENV{HTTP_ACCEPT_LANGUAGE};
     print '<!DOCTYPE html>';
     print <<"EOF";
 <html class="kalliope"><HEAD><TITLE>$titleForWindow</TITLE>
+<!-- HTTP ACCEPT-LANGUAGE: $http_accept_language --> 
 <LINK REL="Shortcut Icon" HREF="http://www.kalliope.org/favicon.ico">
 <link rel="apple-touch-icon" href="http://www.kalliope.org/gfx/icons/iphone-icon.png">
 <meta name="viewport" content="width=device-width; initial-scale=1.0" />
@@ -339,8 +346,24 @@ EOF
  
     print '<center>';
 
-    if (0) {
-    print <<"GOOGLEADS";
+    if (isMobile()) {
+        print <<"GOOGLEADS";
+<script type="text/javascript"><!--
+  // XHTML should not attempt to parse these strings, declare them CDATA.
+  /* <![CDATA[ */
+  window.googleAfmcRequest = {
+      client: 'ca-mb-pub-3823256275585089',
+	      format: '320x50_mb',
+	      output: 'HTML',
+	      slotname: '7470613725',
+	    };
+  /* ]]> */
+//--></script>
+<script type="text/javascript"    src="http://pagead2.googlesyndication.com/pagead/show_afmc_ads.js"></script>
+
+GOOGLEADS
+    } else {	
+        print <<"GOOGLEADS";
     <script type="text/javascript"><!--
     google_ad_client = "pub-3823256275585089";
     //468x60, created 11/12/07
