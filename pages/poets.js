bcce9445d8b74bbc7a19656b1ee55d1d3d8109e2
Jesper Christensen <jesper@kalliope.org>
2017-05-16 15:34:33 +0200

    Load and show data

--- pages/poets.js
+++ pages/poets.js
@@ -1,12 +1,19 @@
 import Link from 'next/link'
 import Head from '../components/head'
 import Nav from '../components/nav'
+import 'isomorphic-fetch'
 
 export default class extends React.Component {
-  static getInitialProps ({ query: { lang } }) {
-    return { lang }
+  static async getInitialProps ({ query: { lang } }) {
+    const res = await fetch('http://localhost:3000/static/api/poets-dk.json')
+    const poets = await res.json()
+    return { lang, poets }
   }
   render() {
+    const { lang, poets } = this.props;
+    const list = poets.map( (poet, i) => {
+      return <div key={i}>{poet.lastName}</div>;
+    })
     return (
     <div>
       <Head title="Digtere - Kalliope" />
@@ -15,8 +22,9 @@ export default class extends React.Component {
       <div className="hero">
         <h1 className="title">Poets {this.props.lang}!</h1>
         <p className="description">Here goes the list of poets</p>
+        {list}
       </div>
     </div>
-  )
-}
+    )
+  }
 }
