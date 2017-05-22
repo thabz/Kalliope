04f9789f721aaa9e6e12d4bdbf3ea9d37d29354b
Jesper Christensen <jesper@kalliope.org>
2017-05-22 14:56:47 +0200

    Custom document to attain lang attr on html

---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+// ./pages/_document.js
+import Document, { Head, Main, NextScript } from 'next/document';
+import flush from 'styled-jsx/server';
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage, query: { lang } }) {
+    const { html, head, errorHtml, chunks } = renderPage();
+    const styles = flush();
+    return { lang, html, head, errorHtml, chunks, styles };
+  }
+
+  render() {
+    return (
+      <html lang={this.props.lang}>
+        <Head>
+          <style>{`body { margin: 0 } /* custom! */`}</style>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
