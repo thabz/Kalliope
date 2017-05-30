2695acfaed50363ae70a5b663ee07fe8c2f8a28b
Jesper Christensen <jesper@kalliope.org>
2017-05-30 10:55:25 +0200

    Sort Aa correctly

--- pages/helpers/sorting.test.js
+++ pages/helpers/sorting.test.js
@@ -10,4 +10,11 @@ describe('sectionsByTitle', () => {
     const sorted = data.sort(Sorting.sectionsByTitle).map(x => x.title);
     expect(sorted).toEqual(['A', 'B', 'C', 'Æ', 'Ø', 'Å']);
   });
+  it('sorts ÆØAa correctly', () => {
+    const data = ['Ø', 'A', 'Æ', 'B', 'Aa', 'C'].map(x => {
+      return { title: x };
+    });
+    const sorted = data.sort(Sorting.sectionsByTitle).map(x => x.title);
+    expect(sorted).toEqual(['A', 'B', 'C', 'Æ', 'Ø', 'Aa']);
+  });
 });
