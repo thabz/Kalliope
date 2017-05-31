9ac955c0b33ddbfc3760edb81e0ff9780204f879
Jesper Christensen <jesper@kalliope.org>
2017-05-31 10:39:56 +0200

    Only navigator.serviceWorker when available

--- static/register-sw.js
+++ static/register-sw.js
@@ -1,7 +1,6 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js', { scope: '/' });
+  navigator.serviceWorker.ready.then(function(registration) {
+    console.log('Service Worker Ready');
+  });
 }
-
-navigator.serviceWorker.ready.then(function(registration) {
-  console.log('Service Worker Ready');
-});
