5d56d1382e9fc76273a52e4d79e34246f3dbbdb6
Jesper Christensen <jesper@kalliope.org>
2017-06-01 14:28:07 +0200

    Some responsive layout
    Also moving CSS into the components

--- components/sectionedlist.js
+++ components/sectionedlist.js
@@ -28,6 +28,33 @@ export default class extends React.Component {
     return (
       <div className="two-columns">
         {renderedGroups}
+        <style jsx>{`
+            div.two-columns {
+              width: 100%;
+              columns: 2;
+              column-gap: 20px;
+            }
+
+            @media (max-width: 480px) {
+              div.two-columns {
+                columns: 1 !important;
+              }
+            }
+
+            :global(.list-section) {
+              break-inside: avoid;
+            }
+
+            :global(.list-section), :global(.list-section-line) {
+              line-height: 1.5;
+            }
+            /* Fix vertical aligment layout problem in Safari and Chrome */
+            :global(.list-section:before) {
+              content: '';
+              display: block;
+              height: 1px;
+            }
+        `}</style>
       </div>
     );
   }
