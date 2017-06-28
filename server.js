9d0c87f3339d76ae6e11eaf456b58ce90ce57a09
Jesper Christensen <jesper@kalliope.org>
2017-06-28 14:31:25 +0200

    Revert "New custom server"
    This reverts commit ec2072bda330c74116e4459abac31600d00ddd77.

--- server.js
+++ server.js
@@ -6,32 +6,26 @@ const { join } = require('path');
 const { createServer } = require('http');
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
-const handler = routes.getRequestHandler(app, ({ req, res, route, query }) => {
-  app.render(req, res, route.page, query);
-});
+const handler = routes.getRequestHandler(app);
 
 const rootStaticFiles = ['/sw.js'];
 
 app.prepare().then(() => {
-  createServer(handler).listen(3000);
-});
+  createServer((req, res) => {
+    const { pathname, query } = parse(req.url, true);
 
-// app.prepare().then(() => {
-//   createServer((req, res) => {
-//     const { pathname, query } = parse(req.url, true);
-//
-//     if (rootStaticFiles.indexOf(pathname) > -1) {
-//       const path = join(__dirname, 'static', pathname);
-//       app.serveStatic(req, res, path);
-//       return;
-//     } else {
-//       handler(req, res);
-//     }
-//   }).listen(3000, err => {
-//     if (err) throw err;
-//     console.log('> Ready on http://localhost:3000');
-//   });
-// });
+    if (rootStaticFiles.indexOf(pathname) > -1) {
+      const path = join(__dirname, 'static', pathname);
+      app.serveStatic(req, res, path);
+      return;
+    } else {
+      handler(req, res);
+    }
+  }).listen(3000, err => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
+});
 
 // Assigning `query` into the params means that we still
 // get the query string passed to our application
