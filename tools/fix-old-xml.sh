b2ddb4efa02bb22261881719ace401ddc86b218f
Jesper Christensen <jesper@kalliope.org>
2017-07-01 09:11:55 +0200

    Handle poem links and </A>

--- tools/fix-old-xml.sh
+++ tools/fix-old-xml.sh
@@ -6,7 +6,9 @@ do
     replaceregexp 's/<\/I>/<\/i>/g' $file;
     replaceregexp 's/<B>/<b>/g' $file;
     replaceregexp 's/<\/B>/<\/b>/g' $file;
+    replaceregexp 's/<\/A>/<\/a>/g' $file;
     replaceregexp 's/<a +v=([^>]*)>/<a work=$1>/gi' $file;
     replaceregexp 's/<a +f=([^>]*)>/<a poet=$1>/gi' $file;
+    replaceregexp 's/<a +d=([^>]*)>/<a poem=$1>/gi' $file;
     replaceregexp 's/<a  href/<a href/gi' $file;
 done
