3a502d1d889ecc7f5dcbfd9a52c05cedad9c20a6
Jesper Christensen <jesper@kalliope.org>
2017-08-23 13:37:59 +0200

    Luft under underoverskrifter
    Fikser #192

--- pages/text.js
+++ pages/text.js
@@ -73,7 +73,7 @@ class TextHeading extends React.Component {
             font-size: 1.05em;
             line-height: 1.05em;
             font-weight: normal;
-            margin: 0;
+            margin: 0 0 15px 0;
             padding: 0;
           }
           .text-heading {
