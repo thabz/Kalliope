a03f35ea94c16562f3d811a9fe14cdc0bef43313
Jesper Christensen <jesper@kalliope.org>
2017-12-07 14:27:07 +0100

    Flere regler

--- tools/fraktur-ocr-cleanup.rb
+++ tools/fraktur-ocr-cleanup.rb
@@ -1,3 +1,5 @@
+s/oeg/æg/
+s/hoi/høi/
 s/folg/følg/
 s/Skjon/Skjøn/
 s/ce/æ/
@@ -12,3 +14,97 @@ s/drom/drøm/
 s/msrk/mørk/
 s/gjor/gjør/
 s/sial/skal/
+s/Ncer/Nær/
+s/M /M/
+s/scrt/sæt/
+s/ stal/ skal/
+s/stodt/stødt/
+s/storre/større/
+s/Oie/Øie/
+s/bce/bæ/
+s/Son /Søn /
+s/Hoi/Høi/
+s/ Dod/ Død/
+s/S a a /Saa /
+s/S a a/Saa/
+s/sortal/fortæl/
+s/ sor / for /
+s/hsie/høie/
+s/forst /først /
+s/forste /første /
+s/speede/spæde/
+s/Lce/Læ/
+s/troeng/træng/
+s/rce/ræ/
+s/loft/løft/
+s/giore/giøre/
+s/flut/slut/
+s/ flue/ skue/
+s/ fode / føde /
+s/Hpi/Høi/
+s/fodt/født/
+s/ipn/iøn/
+s/mce/mæ/
+s/cen/æn/
+s/sæne/scene/
+s/Sæne/Scene/
+s/Rost/Røst/
+s/Fce/Fæ/
+s/cel/æl/
+s/ceb/æb/
+s/ced/æd/
+s/cer/ær/
+s/cet/æt/
+s/ceg/æg/
+s/cem/æm/
+s/cev/æv/
+s/crd/æd/
+s/crb/æb/
+s/crm/æm/
+s/crg/æg/
+s/crf/æf/
+s/crv/æv/
+s/crk/æk/
+s/crl/æl/
+s/crn/æn/
+s/crt/æt/
+s/crs/æs/
+s/Bp/Bø/
+s/Oin/Øin/
+s/skion/skiøn/
+s/sierne/fierne/
+s/sorge/sørge/
+s/ sode / søde /
+s/moel/mæl/
+s/troste/trøste/
+s/troste/trøste/
+s/fpl/føl/
+s/fsr/før/
+s/elst/elsk/
+s/Jnd/Ind/
+s/Jf/If/
+s/ZE/Æ/
+s/Sial/Siæl/
+s/Dine/Øine/
+s/Onske/Ønske/
+s/Drsm/Drøm/
+s/Bon /Bøn /
+s/Bonner/Bønner/
+s/Bonder/Bønder/
+s/Sporg/Spørg/
+s/Folg/Følg/
+s/Fodsel/Fødsel/
+s/Boek/Bæk/
+s/sporg/spørg/
+s/labe/læbe/
+s/overste/øverste/
+s/storst/størst/
+s/strom/strøm/
+s/ hore / høre /
+s/fkab/skab/
+s/beromte/berømte/
+s/ soge / søge /
+s/ soger / søger /
+
+
+
