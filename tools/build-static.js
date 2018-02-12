f43a9543533d100ebc79dd817231c78c5fedeba8
Jesper Christensen <jesper@kalliope.org>
2018-02-12 13:33:47 +0100

    Et sitemap pr. digter

--- tools/build-static.js
+++ tools/build-static.js
@@ -1761,10 +1761,26 @@ const build_image_thumbnails = () => {
 };
 
 const build_sitemap_xml = collected => {
+  safeMkdir(`static/sitemaps`);
+
+  const write_sitemap = (filename, urls) => {
+    let xmlUrls = urls.map(url => {
+      return `  <url><loc>${url}</loc></url>`;
+    });
+    let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
+    xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+    xml += xmlUrls.join('\n');
+    xml += '\n</urlset>\n';
+    writeText(filename, xml);
+  };
+
   let urls = [];
   ['da', 'en'].forEach(lang => {
     urls.push(`https://kalliope.org/${lang}/`);
     urls.push(`https://kalliope.org/${lang}/keywords`);
+    collected.keywords.forEach((keyword, keywordId) => {
+      urls.push(`https://kalliope.org/${lang}/keyword/${keywordId}`);
+    });
     collected.poets.forEach((poet, poetId) => {
       urls.push(`https://kalliope.org/${lang}/bio/${poetId}`);
       if (poet.has_poems) {
@@ -1776,19 +1792,55 @@ const build_sitemap_xml = collected => {
       }
       if (poet.has_works) {
         urls.push(`https://kalliope.org/${lang}/works/${poetId}`);
-        collected.workids.get(poetId).forEach(workId => {
-          urls.push(`https://kalliope.org/${lang}/work/${poetId}/${workId}`);
-        });
+        collected.workids.get(poetId).forEach(workId => {});
       }
     });
   });
-  let xmlUrls = urls.map(url => {
-    return `  <url><loc>${url}</loc></url>`;
+  write_sitemap('static/sitemaps/global.xml', urls);
+
+  collected.poets.forEach((poet, poetId) => {
+    const filenames = collected.workids
+      .get(poetId)
+      .map(workId => `fdirs/${poetId}/${workId}.xml`);
+    if (!isFileModified(...filenames)) {
+      return;
+    }
+    const poet_text_urls = [];
+    ['da', 'en'].forEach(lang => {
+      collected.workids.get(poetId).forEach(workId => {
+        poet_text_urls.push(
+          `https://kalliope.org/${lang}/work/${poetId}/${workId}`
+        );
+        const filename = `fdirs/${poetId}/${workId}.xml`;
+        let doc = loadXMLDoc(filename);
+        if (doc == null) {
+          console.log("Couldn't load", filename);
+        }
+        doc.find('//poem').forEach(part => {
+          const textId = part.attr('id').value();
+          poet_text_urls.push(`https://kalliope.org/${lang}/text/${textId}`);
+        });
+      });
+    });
+    write_sitemap(`static/sitemaps/${poetId}.xml`, poet_text_urls);
   });
+
+  const sitemaps_urls_xml = Array.from(collected.poets.values())
+    .filter(poet => poet.has_works)
+    .map(poet => {
+      return `https://kalliope.org/static/sitemaps/${poet.id}.xml`;
+    })
+    .map(url => {
+      return `  <url><loc>${url}</loc></url>`;
+    });
+  sitemaps_urls_xml.push(
+    `  <url><loc>https://kalliope.org/static/sitemaps/global.xml</loc></url>`
+  );
   let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
-  xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
-  xml += xmlUrls.join('\n');
-  xml += '\n</urlset>\n';
+  xml +=
+    '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">>\n';
+  xml += sitemaps_urls_xml.join('\n');
+  xml += '\n</sitemapindex>\n';
   writeText('static/sitemap.xml', xml);
 };
 
