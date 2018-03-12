904166326e65b6f2d15b91602443e192b9c87b2d
Jesper Christensen <jesper@kalliope.org>
2018-03-12 09:49:49 +0100

    Flere ICU tests

--- __tests__/icu.js
+++ __tests__/icu.js
@@ -1,5 +1,11 @@
 describe('ICU checks', () => {
-  it(`Har ICU installeret`, () => {
+  // https://nodejs.org/api/intl.html#intl_providing_icu_data_at_runtime
+  it(`Har ICU overhovedet`, () => {
+    expect(typeof Intl).toBe('object');
+    expect(typeof process.versions.icu).toBe('string');
+  });
+
+  it(`Har fuld ICU`, () => {
     // https://nodejs.org/api/intl.html#intl_providing_icu_data_at_runtime
     const hasFullICU = (() => {
       try {
