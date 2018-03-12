a5cb1bf77e773ffce8ce33820e983ce27fac1875
Jesper Christensen <jesper@kalliope.org>
2018-03-12 13:16:53 +0100

    Systematisk fejlfinding

--- __tests__/poem-lines.js
+++ __tests__/poem-lines.js
@@ -1,3 +1,13 @@
+import {
+  loadXMLDoc,
+  safeGetText,
+  loadText,
+  fileExists,
+} from '../tools/libs/helpers.js';
+
+function flatten(arr) {
+  return [].concat(...arr);
+}
 // Regulære expressions som fanger typiske fejl i vores XML.
 const regexps = [
   /^,[a-zæøåA-ZÆØÅ]/,
@@ -5,7 +15,7 @@ const regexps = [
   /^\.[a-zæøåA-ZÆØÅ]/,
   /^-[a-zæøåA-ZÆØÅ]/,
   /mmm/,
-  /iii/, // Problematisk da den rammer lowercase romertal. Fiks fejlere og drop reglen.
+  ///iii/, // Problematisk da den rammer lowercase romertal. Fiks fejlere og drop reglen.
   /lll/,
   /aaa/,
   /sss/,
@@ -13,8 +23,39 @@ const regexps = [
 ];
 // TODO: Hver regel kunne have nogle white-list regexps, som angiver undtagelser. F.eks. reglen /aaa/ kunne undtagelsen /Smaaalfer/
 
-describe('Ingen digtere har samme id', () => {
-  it(`Alle digtere har forskellige id`, () => {
-    expect(1).toBe(1);
+describe('Check workfiles', () => {
+  const doc = loadXMLDoc('data/poets.xml');
+  let filenames = doc.find('/persons/person').map(person => {
+    const poetId = person.attr('id').value();
+    const workIds = person.get('works');
+    let items = workIds
+      ? workIds
+          .toString()
+          .replace('<works>', '')
+          .replace('</works>', '')
+          .replace('<works/>', '')
+          .trim()
+          .split(',')
+          .filter(x => x.length > 0)
+      : [];
+    return items.map(w => `${poetId}/${w}.xml`);
+  });
+  filenames = flatten(filenames);
+  filenames.forEach(filename => {
+    const fullpath = `fdirs/${filename}`;
+    const data = loadText(fullpath);
+    it(`Workfile ${filename} is fine`, () => {
+      expect(fileExists(fullpath)).toBeTruthy;
+      expect(data.length > 0);
+      regexps.forEach(regexp => {
+        if (regexp.test(data)) {
+          const matches = data.match(regexp);
+          const m = matches[0].replace('\n', '');
+          console.log(matches[0]);
+          //fail(`'${matches[0]}' found in xml`);
+          fail(`'${regexp.toString()}' found in xml`);
+        }
+      });
+    });
   });
 });
