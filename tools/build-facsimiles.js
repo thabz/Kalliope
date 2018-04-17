a1b530fc387923572bc9a4a1c657b071f097f8a6
Jesper Christensen <jesper@kalliope.org>
2018-04-17 16:23:35 +0200

    Convert to jpegs done

--- tools/build-facsimiles.js
+++ tools/build-facsimiles.js
@@ -3,8 +3,27 @@ const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp');
 const { safeMkdir } = require('./libs/helpers.js');
-
 const dirname = 'static/facsimiles';
+
+// Place pdf-files in static/facsimiles/<poetname>/<workid>.pdf
+// For each pdf file this script creates a folder static/facsimiles/<poetname>/<workid>
+// containing jpg-files for each page in the pdf-file, named <workid>-<pagenumber>.jpg
+// where <pagenumber> is 000, 001, 002, etc.
+
+// Convert all ppm- and pbm-files in a folder to jpg
+const folderToJpeg = imagesDir => {
+  console.log(`Converting ${imagesDir} to jpeg`);
+  fs
+    .readdirSync(imagesDir)
+    .filter(f => f.endsWith('.pbm') || f.endsWith('.ppm'))
+    .forEach(srcFilename => {
+      const srcPath = path.join(imagesDir, srcFilename);
+      const destPath = srcPath.replace(/.pbm/, '.jpg').replace(/.ppm/, '.jpg');
+      exec(`convert "${srcPath}" "${destPath}"`, () => {
+        fs.unlinkSync(srcPath);
+      });
+    });
+};
 
 //exec(`convert -density 300 ${pdfFilename} 1852-%03d.jpg`);
 
@@ -23,9 +42,15 @@ poetDirs.forEach(poetDir => {
       const parts = fullFilename.split('/');
       const workId = parts[parts.length - 1].replace('.pdf', '');
       const imagesDir = path.join(poetDir, workId);
-      safeMkdir(imagesDir);
-      const imagesPrefix = path.join(imagesDir, workId);
-      console.log(`Extracting from ${fullFilename}`);
-      exec(`pdfimages -jp2 ${fullFilename} ${imagesPrefix}`);
+      // Skip if imagesDir exists
+      if (!fs.existsSync(imagesDir)) {
+        safeMkdir(imagesDir);
+        const imagesPrefix = path.join(imagesDir, workId);
+        console.log(`Extracting from ${fullFilename}`);
+        // Extract ppm-files
+        exec(`pdfimages -jp2 ${fullFilename} ${imagesPrefix}`, () => {
+          folderToJpeg(imagesDir);
+        });
+      }
     });
 });
