7cdd5c2b6e013be5935a03a4a2c352e8192f1071
Jesper Christensen <jesper@kalliope.org>
2019-02-18 11:22:24 +0100

    Skip ovaler

--- tools/copy-screensaver-images.sh
+++ tools/copy-screensaver-images.sh
@@ -2,13 +2,8 @@
 
 destdir="/Users/jec/Library/Mobile Documents/com~apple~CloudDocs/Photos/Kalliope screensaver" 
 
-find static/images/* -type f -regex ".*images/[^/]*/[^/]*jpg" -size +300k -and -not -regex ".*square.*" -and -not -regex ".*webp" | while IFS= read -r pathname; do
+find static/images/* -type f -regex ".*images/[^/]*/[^/]*jpg" -size +300k -and -not -regex ".*square.*" -and -not -regex ".*webp" -and -not -regex ".*oval.*"| while IFS= read -r pathname; do
     outname=`echo $pathname | sed 's/\//-/g'`
     cp "$pathname" "$destdir/$outname"
-    
- #   base=$(basename "$pathname"); name=${base%.*}; ext=${base##*.}
- #   mv "$pathname" "foo/${name}.bar.${ext}"
 done
-
-#find static/images/* -type f -regex ".*images/[^/]*/[^/]*jpg" -size +300k -and -not -regex ".*square.*" -and -not -regex ".*webp" -exec cp {} "/Users/jec/Library/Mobile Documents/com~apple~CloudDocs/Photos/Kalliope screensaver" \;
 
