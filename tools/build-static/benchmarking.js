30fc3af253a9c5fc8d231f57044deec90068e8ed
Jesper Christensen <jesper@kalliope.org>
2020-02-22 03:13:00 +0100

    Done

--- tools/build-static/benchmarking.js
+++ tools/build-static/benchmarking.js
@@ -2,7 +2,7 @@ let b_keys = [];
 let b_millis = {};
 // Benchmarking
 const b = async (name, f, args) => {
-  console.log(`Begin ${name}`);
+  console.log(`${name}...`);
   const beforeMillis = Date.now();
   const result = f(args);
   const afterMillis = Date.now();
@@ -11,17 +11,17 @@ const b = async (name, f, args) => {
     b_millis[name] = 0;
   }
   b_millis[name] = b_millis[name] + afterMillis - beforeMillis;
-  console.log(`End ${name}`);
   return result;
 };
 const print_benchmarking_results = () => {
   let sum = 0;
+  console.log('\nSTATS');
   b_keys.forEach(key => {
     const millis = b_millis[key];
     sum += millis;
     console.log(`${key}: ${millis}ms`);
   });
-  console.log(`sum: ${sum}ms`);
+  console.log(`SUM: ${sum}ms`);
 };
 
 module.exports = {
