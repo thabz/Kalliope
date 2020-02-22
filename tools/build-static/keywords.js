4eaa9dd564cca765e78f230fbf21ba1596ae25be
Jesper Christensen <jesper@kalliope.org>
2020-02-22 21:23:53 +0100

    get_pictures er async

--- tools/build-static/keywords.js
+++ tools/build-static/keywords.js
@@ -14,7 +14,7 @@ const {
   getChildByTagName,
 } = require('./xml.js');
 
-const build_keywords = collected => {
+const build_keywords = async collected => {
   safeMkdir('static/api/keywords');
   let collected_keywords = new Map(loadCachedJSON('collected.keywords') || []);
   const folder = 'data/keywords';
@@ -25,48 +25,51 @@ const build_keywords = collected => {
   if (collected_keywords.size === 0 || isFileModified(...filenames)) {
     collected_keywords = new Map();
     let keywords_toc = new Array();
-    filenames.map(path => {
-      if (!path.endsWith('.xml')) {
-        return;
-      }
-      const doc = loadXMLDoc(path);
-      const keyword = getChildByTagName(doc, 'keyword');
-      const head = getChildByTagName(keyword, 'head');
-      const body = getChildByTagName(keyword, 'body');
-      const id = safeGetAttr(keyword, 'id');
-      const is_draft = safeGetAttr(keyword, 'draft') === 'true';
-      const title = safeGetText(head, 'title');
-      const pictures = get_pictures(
-        head,
-        '/static/images/keywords',
-        path,
-        collected
-      );
-      const author = safeGetText(head, 'author');
-      const rawBody = safeGetInnerXML(body);
-      const content_html = htmlToXml(rawBody, collected);
-      const has_footnotes =
-        rawBody.indexOf('<footnote') !== -1 || rawBody.indexOf('<note') !== -1;
-      const data = {
-        id,
-        title,
-        is_draft,
-        author,
-        pictures,
-        has_footnotes,
-        content_lang: 'da',
-        content_html,
-      };
-      keywords_toc.push({
-        id,
-        title,
-        is_draft,
-      });
-      const outFilename = `static/api/keywords/${id}.json`;
-      console.log(outFilename);
-      writeJSON(outFilename, data);
-      collected_keywords.set(id, { id, title });
-    });
+    await Promise.all(
+      filenames.map(async path => {
+        if (!path.endsWith('.xml')) {
+          return;
+        }
+        const doc = loadXMLDoc(path);
+        const keyword = getChildByTagName(doc, 'keyword');
+        const head = getChildByTagName(keyword, 'head');
+        const body = getChildByTagName(keyword, 'body');
+        const id = safeGetAttr(keyword, 'id');
+        const is_draft = safeGetAttr(keyword, 'draft') === 'true';
+        const title = safeGetText(head, 'title');
+        const pictures = await get_pictures(
+          head,
+          '/static/images/keywords',
+          path,
+          collected
+        );
+        const author = safeGetText(head, 'author');
+        const rawBody = safeGetInnerXML(body);
+        const content_html = htmlToXml(rawBody, collected);
+        const has_footnotes =
+          rawBody.indexOf('<footnote') !== -1 ||
+          rawBody.indexOf('<note') !== -1;
+        const data = {
+          id,
+          title,
+          is_draft,
+          author,
+          pictures,
+          has_footnotes,
+          content_lang: 'da',
+          content_html,
+        };
+        keywords_toc.push({
+          id,
+          title,
+          is_draft,
+        });
+        const outFilename = `static/api/keywords/${id}.json`;
+        console.log(outFilename);
+        writeJSON(outFilename, data);
+        collected_keywords.set(id, { id, title });
+      })
+    );
     writeCachedJSON('collected.keywords', Array.from(collected_keywords));
     const outFilename = `static/api/keywords.json`;
     console.log(outFilename);
