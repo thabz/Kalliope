8bda810affeb94d318bf4495f48b0272d77a90ae
Jesper Christensen <jesper@kalliope.org>
2020-02-22 14:01:46 +0100

    Fix fonte

--- pages/mentions.js
+++ pages/mentions.js
@@ -64,7 +64,7 @@ class Section extends React.Component {
         <TwoColumns>{items}</TwoColumns>
         <style jsx>{`
           h3 {
-            font-weight: normal;
+            font-weight: 300;
             font-size: 18px;
             border-bottom: 1px solid black;
           }
