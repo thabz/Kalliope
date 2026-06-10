f74a933190827322c3d27779c18a2c768779e994
Jesper Christensen <jesper@kalliope.org>
2026-06-10 22:35:36 +0200

    Upgrade to Next 8

--- next.config.js
+++ next.config.js
@@ -1,9 +1,52 @@
+const { createHash } = require('crypto')
+
+class NodeSafeHashedChunkIdsPlugin {
+  constructor(buildId) {
+    this.buildId = buildId
+  }
+
+  apply(compiler) {
+    compiler.hooks.compilation.tap('NodeSafeHashedChunkIdsPlugin', compilation => {
+      compilation.hooks.beforeChunkIds.tap(
+        'NodeSafeHashedChunkIdsPlugin',
+        chunks => {
+          for (const chunk of chunks) {
+            if (chunk.id === null && chunk.name) {
+              const id = chunk.name.replace(this.buildId, '')
+              chunk.id = createHash('md4')
+                .update(id)
+                .digest('hex')
+                .substr(0, 4)
+              continue
+            }
+
+            const ids = [...chunk.modulesIterable]
+              .map(m => m.id)
+              .sort()
+            const h = createHash('md4')
+            ids.forEach(id => h.update(String(id)))
+            chunk.id = h.digest('hex').substr(0, 4)
+          }
+        }
+      )
+    })
+  }
+}
+
 module.exports = {
   webpack: (config) => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
       fs: 'empty'
     }
+
+    config.plugins = config.plugins.map(plugin => {
+      if (plugin && plugin.constructor.name === 'HashedChunkIdsPlugin') {
+        return new NodeSafeHashedChunkIdsPlugin(plugin.buildId)
+      }
+
+      return plugin
+    })
 
     return config
   }
