513a468e1f2889b8a800c6331232566853f2735d
Jesper Christensen <jesper@kalliope.org>
2026-06-14 08:00:48 +0200

    Convert React.Component class to funtions (#1104)

--- components/head.js
+++ components/head.js
@@ -1,5 +1,4 @@
 import NextHead from 'next/head';
-import React from 'react';
 
 const urlPrefix = 'https://kalliope.org';
 const defaultDescription =
@@ -7,79 +6,76 @@ const defaultDescription =
 const defaultOGURL = urlPrefix;
 const defaultOGImage = `${urlPrefix}/touch-icon.png`;
 
-export default class Head extends React.Component {
-  render() {
-    const { ogTitle, headTitle, description, url, ogImage, requestPath } =
-      this.props;
-
-    const appleTouchIcons = [180, 152, 120, 76, 60].map((s) => {
-      const x = `${s}x${s}`;
+const Head = ({ ogTitle, headTitle, description, url, ogImage, requestPath }) => {
+  const appleTouchIcons = [180, 152, 120, 76, 60].map((s) => {
+    const x = `${s}x${s}`;
+    return (
+      <link
+        key={x}
+        rel="apple-touch-icon"
+        sizes={x}
+        href={`/apple-touch-icon-${x}.png`}
+      />
+    );
+  });
+  let ogImageAbsolute = ogImage != null ? ogImage : defaultOGImage;
+  if (!ogImageAbsolute.indexOf('http') === 0) {
+    ogImageAbsolute = `${urlPrefix}${ogImageAbsolute}`;
+  }
+  let hreflangs = [];
+  if (requestPath != null) {
+    hreflangs = ['da', 'en'].map((lang) => {
+      const alternatePath = requestPath.replace(/^\/../, '/' + lang);
+      const alternateURL = urlPrefix + alternatePath;
       return (
         <link
-          key={x}
-          rel="apple-touch-icon"
-          sizes={x}
-          href={`/apple-touch-icon-${x}.png`}
+          rel="alternate"
+          hrefLang={lang}
+          href={alternateURL}
+          key={lang}
         />
       );
     });
-    let ogImageAbsolute = ogImage != null ? ogImage : defaultOGImage;
-    if (!ogImageAbsolute.indexOf('http') === 0) {
-      ogImageAbsolute = `${urlPrefix}${ogImageAbsolute}`;
-    }
-    let hreflangs = [];
-    if (requestPath != null) {
-      hreflangs = ['da', 'en'].map((lang) => {
-        const alternatePath = requestPath.replace(/^\/../, '/' + lang);
-        const alternateURL = urlPrefix + alternatePath;
-        return (
-          <link
-            rel="alternate"
-            hrefLang={lang}
-            href={alternateURL}
-            key={lang}
-          />
-        );
-      });
-    }
-    let canonical = null;
-    let ogURL = null;
-    if (requestPath != null) {
-      const url = urlPrefix + requestPath;
-      canonical = <link rel="canonical" href={url} />;
-      ogURL = <meta property="og:url" content={url} />;
-    }
-    return (
-      <NextHead>
-        <meta charSet="UTF-8" />
-        <title>{headTitle || ogTitle || ''}</title>
-        <meta name="description" content={description || defaultDescription} />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        {appleTouchIcons}
-        <link rel="mask-icon" href="/favicon-mask.svg" color="black" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
-        {canonical}
-        {hreflangs}
-        <meta name="theme-color" content="rgb(139, 56, 65)" />
-        <meta property="og:site_name" content="www.kalliope.org" />
-        {/*<meta property="og:url" content={url || defaultOGURL} />*/}
-        <meta property="og:title" content={ogTitle || headTitle || ''} />
-        <meta
-          property="og:description"
-          content={description || defaultDescription}
-        />
-        <meta name="twitter:site" content="@kalliope_org" />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:image" content={ogImageAbsolute} />
-        <meta property="og:image" content={ogImageAbsolute} />
-        <meta property="og:image:width" content="600" />
-        <meta property="og:site_name" content="Kalliope" />
-        <meta property="og:type" content="website" />
-        {ogURL}
-      </NextHead>
-    );
   }
-}
+  let canonical = null;
+  let ogURL = null;
+  if (requestPath != null) {
+    const url = urlPrefix + requestPath;
+    canonical = <link rel="canonical" href={url} />;
+    ogURL = <meta property="og:url" content={url} />;
+  }
+  return (
+    <NextHead>
+      <meta charSet="UTF-8" />
+      <title>{headTitle || ogTitle || ''}</title>
+      <meta name="description" content={description || defaultDescription} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      {appleTouchIcons}
+      <link rel="mask-icon" href="/favicon-mask.svg" color="black" />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="manifest" href="/manifest.json" />
+      {canonical}
+      {hreflangs}
+      <meta name="theme-color" content="rgb(139, 56, 65)" />
+      <meta property="og:site_name" content="www.kalliope.org" />
+      {/*<meta property="og:url" content={url || defaultOGURL} />*/}
+      <meta property="og:title" content={ogTitle || headTitle || ''} />
+      <meta
+        property="og:description"
+        content={description || defaultDescription}
+      />
+      <meta name="twitter:site" content="@kalliope_org" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:image" content={ogImageAbsolute} />
+      <meta property="og:image" content={ogImageAbsolute} />
+      <meta property="og:image:width" content="600" />
+      <meta property="og:site_name" content="Kalliope" />
+      <meta property="og:type" content="website" />
+      {ogURL}
+    </NextHead>
+  );
+};
+
+export default Head;
--- components/worksubtitles.js
+++ components/worksubtitles.js
@@ -1,23 +1,20 @@
-import React from 'react';
 import TextContent from './textcontent.js';
 
-export default class WorkSubtitles extends React.Component {
-  render() {
-    const { work, lang } = this.props;
-    const { year } = work;
-    if (work.subtitles != null && work.subtitles.length > 0) {
-      return work.subtitles.map((subtitle, i) => {
-        return (
-          <div
-            key={'subtitle' + i}
-            style={{ fontSize: '0.8em', marginTop: '1em' }}
-          >
-            <TextContent contentHtml={subtitle} lang={lang} />
-          </div>
-        );
-      });
-    } else {
-      return null;
-    }
+const WorkSubtitles = ({ work, lang }) => {
+  if (work.subtitles != null && work.subtitles.length > 0) {
+    return work.subtitles.map((subtitle, i) => {
+      return (
+        <div
+          key={'subtitle' + i}
+          style={{ fontSize: '0.8em', marginTop: '1em' }}
+        >
+          <TextContent contentHtml={subtitle} lang={lang} />
+        </div>
+      );
+    });
+  } else {
+    return null;
   }
-}
+};
+
+export default WorkSubtitles;
