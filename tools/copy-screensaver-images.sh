f3e79ff33efeb54d0351c5196c0fcaef597b3685
Jesper Christensen <jesper@kalliope.org>
2026-07-21 09:19:20 +0200

    Ryd op i forældede værktøjer (#1285)

--- tools/copy-screensaver-images.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-destdir="/Users/jec/Library/Mobile Documents/com~apple~CloudDocs/Photos/Kalliope screensaver"
-
-find public/images/* -type f -regex ".*images/[^/]*/[^/]*jpg" -size +300k -and -not -regex ".*square.*" -and -not -regex ".*webp" -and -not -regex ".*oval.*"| while IFS= read -r pathname; do
-    outname=`echo $pathname | sed 's/\//-/g'`
-    cp "$pathname" "$destdir/$outname"
-done
